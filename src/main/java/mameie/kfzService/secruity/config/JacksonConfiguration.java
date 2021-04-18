8b76275857b0e660debc241176671dadc51687f2
magantus@googlemail.com <Extrem93!>
2021-04-18 20:50:05 +0200

    add JacksonConfiguration

---
+++ src/main/java/mameie/kfzService/secruity/config/JacksonConfiguration.java
@@ -0,0 +1,43 @@
+package mameie.kfzService.secruity.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfiguration {
+
+    /**
+     * Support for Java date and time API.
+     * @return the corresponding Jackson module.
+     */
+    @Bean
+    public JavaTimeModule javaTimeModule() {
+        return new JavaTimeModule();
+    }
+
+    @Bean
+    public Jdk8Module jdk8TimeModule() {
+        return new Jdk8Module();
+    }
+
+    /*
+     * Support for Hibernate types in Jackson.
+     */
+//    @Bean
+//    public Hibernate5Module hibernate5Module() {
+ //       return new Hibernate5Module();
+ //   }
+
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.findAndRegisterModules();
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        return mapper;
+    }
+}
